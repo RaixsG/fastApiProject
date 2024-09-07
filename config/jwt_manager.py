dc38e7e65304c2ff1f515e08a9f83158adb672b5
RaixsG <jhardelloficial@gmail.com>
2024-09-07 09:20:27 -0500

    feat: actualizar manejo de JWT

--- config/jwt_manager.py
+++ config/jwt_manager.py
@@ -1,8 +1,12 @@
 from jwt import encode, decode
+from config.config import settings
 
 def create_token(data: dict):
-    return encode(payload=data, key="secret", algorithm="HS256")
+    return encode(payload=data, key=settings.secret_key, algorithm=settings.algorithm)
 
 def validate_token(token: str) -> dict:
-    data: dict = decode(token, key="secret", algorithms=["HS256"])
-    return data
+    try:
+        data: dict = decode(token, key=settings.secret_key, algorithms=[settings.algorithm])
+        return data
+    except:
+        return {}
